3c9bff860d7f755789aeca0f13072dd5d74b1b36
Abraham Olaobaju <olaobajua@gmail.com>
2020-06-08 12:42:29 +0100

    Updated Manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-
 {
-    'name': 'Rave Payment Acquirer by Flutterwave',
+    'name': 'Rave Payment Acquirer',
     'category': 'eCommerce',
-    'summary': 'Payment Acquirer:Flutterwave Rave Implementation',
-    'version': '1',
+    'summary': 'Payment Acquirer: Rave Implementation',
+    'version': '2',
+    'license': 'AGPL-3',
+    'author': 'Flutterwave Technology Solutions',
     'website': 'https://rave.flutterwave.com/',
     'description': """Flutterwave Rave Payment Acquirer""",
     'depends': ['payment','website'],
@@ -16,4 +17,5 @@
     'images': ['static/description/icon.png'],
     'installable': True,
     'post_init_hook': 'create_missing_journal_for_acquirers',
+    'uninstall_hook': 'uninstall_hook',
 }
